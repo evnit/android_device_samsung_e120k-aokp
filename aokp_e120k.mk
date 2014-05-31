794a66d8cb05f64501b5517b67fd9743c074d403
evnit <evnithn@gmail.com>
2014-05-31 22:56:25 +0700

    Update and rename cm.mk to aokp_e120k.mk

--- aokp_e120k.mk
+++ aokp_e120k.mk
@@ -1,13 +1,10 @@
 $(call inherit-product, device/samsung/e120k/full_e120k.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common CM stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SHV-E120K TARGET_DEVICE=SHV-E120K BUILD_FINGERPRINT="samsung/SHV-E120K/SHV-E120K:4.4.2/KOT49E/KKMK1:user/release-keys" PRIVATE_BUILD_DESC="SHV-E120K-user 4.4.2 KOT49E KKMK1 release-keys"
 
